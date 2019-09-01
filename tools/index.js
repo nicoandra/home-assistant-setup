27c88987a9825635fd40897810ca5470f6ee04cb
Nico Andrade <nico@nmac.com.ar>
2019-09-01 15:30:36 -0400

    MqttTools and config

--- tools/index.js
+++ tools/index.js
@@ -1,24 +1,82 @@
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://mosquitto')
 
-const timeouts = {};
+/*const timeouts = {};
+const hitCounters = {};
+*/
+
+const topics = {};
 
 client.on('connect', function () {
     console.log("Connected!");
-    client.subscribe('stat/sonoff/switch/dev/RESULT', function (err) {})
-    client.subscribe('stat/sonoff/switch/office/RESULT', function (err) {})
-    client.subscribe('stat/sonoff/switch/living/RESULT', function (err) {})
+    topics['stat/sonoff/switch/dev/RESULT'] = new topic('stat/sonoff/switch/dev/RESULT');
+    topics['stat/sonoff/switch/office/RESULT'] = new topic('stat/sonoff/switch/office/RESULT');
+    topics['stat/sonoff/switch/living/RESULT'] = new topic('stat/sonoff/switch/living/RESULT');
 })
 
 client.on('message', function (topic, message) {
-    if (timeouts[topic]) {
-        clearTimeout(timeouts[topic]);
-    }
-
-    timeouts[topic] = setTimeout(()=> {
-        client.publish(topic + '/filtered', message);
-        console.log("Sent..." , message);
-    }, 500)
-    // console.log("Queued..." , message.toString());
+    topics[topic].queue(message);
 })
 
+const topic = class {
+    constructor(topic) {
+        this.topic = topic;
+        this.timeoutId = null;
+        this.messageQueue = [];
+
+        client.subscribe(topic, function (err) {
+            console.log('Subscribed to topic', topic);
+        })
+    }
+
+    parse(rawMessage) {
+        try {
+            return JSON.parse(rawMessage);
+        } catch (exception) {
+            return message;
+        }     
+    }
+
+    queue(message) {
+        const messageToQueue = this.parse(message);
+
+        this.messageQueue.push(messageToQueue);
+        console.log("queueing ", messageToQueue);
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+        }
+
+        this.timeoutId = setTimeout(() => {
+            this.sendTheAppropriateMessage();
+        }, 500);
+    }
+
+    sendTheAppropriateMessage() {
+        if (this.sequenceOfClicks()) {
+            return this.sendSequenceOfClicks();
+        }
+
+        this.sendRaw(this.messageQueue.pop());
+        this.messageQueue = [];
+    }
+
+    sequenceOfClicks() {
+        if(this.messageQueue.length === 1) {
+            return false;
+        }
+
+        const mirror = this.messageQueue.filter((entry) => !(entry['Dimmer'] || false));
+        return mirror.length === this.messageQueue.length;
+    }
+
+    sendSequenceOfClicks() { 
+        this.sendRaw({ clickCounter: this.messageQueue.length });
+        this.messageQueue = [];
+    }
+
+    sendRaw(message) {
+        const messageToSend = typeof message === 'String' ? message : JSON.stringify(message);
+        console.log('%s >> %s', this.topic, messageToSend);
+        client.publish(this.topic + '/filtered', messageToSend);
+    }
+}
