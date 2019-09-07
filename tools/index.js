88d8022ae0e776b1ea6a32ce0d982b2d70f5bcac
Nico Andrade <nico@nmac.com.ar>
2019-09-07 17:52:37 -0400

    Multi-click office and living dimmer

--- tools/index.js
+++ tools/index.js
@@ -5,28 +5,65 @@ const client  = mqtt.connect('mqtt://mosquitto')
 const hitCounters = {};
 */
 
+const timeWindow = 500;
 const topics = {};
+
+const moesDimmers = (process.env['ENABLED_DIMMERS'] || 'dev').split(',');
 
 client.on('connect', function () {
     console.log("Connected!");
-    topics['stat/sonoff/switch/dev/RESULT'] = new topic('stat/sonoff/switch/dev/RESULT');
-    topics['stat/sonoff/switch/office/RESULT'] = new topic('stat/sonoff/switch/office/RESULT');
-    topics['stat/sonoff/switch/living/RESULT'] = new topic('stat/sonoff/switch/living/RESULT');
+    moesDimmers.forEach((dimmerName) => {
+        topics[dimmerName] = new topic(dimmerName);
+    })
 })
 
 client.on('message', function (topic, message) {
-    topics[topic].queue(message);
+    // If the topic starts with stat => Device to Server. Use normal queue here
+    if (topic.startsWith('stat')) {
+        moesDimmers
+            .filter((dimmerName) => topic.includes('/' + dimmerName + '/'))
+            .map((dimmerName) => { topics[dimmerName].queue(message)});
+
+        return ;
+    }
+    
+    if (topic.startsWith('cmnd')) {
+        const sendTo = topic.replace('/filtered','');
+        console.log("A command received in %s, sending it to %s", topic, sendTo);
+
+        const affectedDimmers = moesDimmers.filter((dimmerName) => topic.includes('/' + dimmerName + '/'));
+
+        console.log("Affected dimmers", affectedDimmers);
+        affectedDimmers.map((dimmerName) => { topics[dimmerName].increaseIgnoreCounter()});
+
+        client.publish(sendTo, message);
+            
+        return ;
+    }
 })
 
 const topic = class {
-    constructor(topic) {
-        this.topic = topic;
+    constructor(name) {
+        this.name = name
+        this.topic = 'stat/sonoff/switch/'+name+'/RESULT';
+        this.commandTopic = 'cmnd/sonoff/switch/'+name+'/POWER/filtered'
         this.timeoutId = null;
         this.messageQueue = [];
+        this.ignoreCounter = 0;
 
-        client.subscribe(topic, function (err) {
-            console.log('Subscribed to topic', topic);
+        client.subscribe(this.topic, (err) => {
+            console.log('Subscribed to topic', this.topic);
         })
+
+        client.subscribe(this.commandTopic, (err)  => {
+            console.log('Subscribed to topic', this.commandTopic);
+        })        
+    }
+
+    increaseIgnoreCounter() {
+        this.ignoreCounter++;
+        console.log('Increasing ignore counter of %s to %d', this.name, this.ignoreCounter);
+        
     }
 
     parse(rawMessage) {
@@ -38,8 +75,20 @@ const topic = class {
     }
 
     queue(message) {
+
         const messageToQueue = this.parse(message);
 
+        if (messageToQueue['Command'] == 'Unknown') { 
+            console.log("Ignore unkown command");
+            return ;
+        }
+
+        if (this.ignoreCounter-- > 0) {
+            console.log('Voided message "%s" in %s, counter is %d', message, this.topic, this.ignoreCounter);
+            return;
+        }
+
+        this.ignoreCounter = 0;
         this.messageQueue.push(messageToQueue);
         console.log("queueing ", messageToQueue);
         if (this.timeoutId) {
@@ -47,16 +96,16 @@ const topic = class {
         }
 
         this.timeoutId = setTimeout(() => {
-            this.sendTheAppropriateMessage();
-        }, 500);
+            this.sendTheAppropriateMessageToServer();
+        }, timeWindow);
     }
 
-    sendTheAppropriateMessage() {
+    sendTheAppropriateMessageToServer() {
         if (this.sequenceOfClicks()) {
-            return this.sendSequenceOfClicks();
+            this.sendSequenceOfClicksToServer();
+        } else {
+            this.sendRawToServer(this.messageQueue.pop());
         }
-
-        this.sendRaw(this.messageQueue.pop());
         this.messageQueue = [];
     }
 
@@ -65,18 +114,18 @@ const topic = class {
         return mirror.length === this.messageQueue.length;
     }
 
-    sendSequenceOfClicks() { 
-        this.sendRaw({ clickCounter: this.messageQueue.length });
+    sendSequenceOfClicksToServer() { 
+        this.sendRawToServer({ clickCounter: this.messageQueue.length });
 
-        if(this.messageQueue.length === 1) {
-            this.sendRaw(this.messageQueue[0]);
+        if(false && this.messageQueue.length === 1) {
+            this.sendRawToServer(this.messageQueue[0]);
         }        
         this.messageQueue = [];
     }
 
-    sendRaw(message) {
+    sendRawToServer(message) {
         const messageToSend = typeof message === 'String' ? message : JSON.stringify(message);
-        console.log('%s >> %s', this.topic, messageToSend);
+        console.log('%s >> %s', this.topic + '/filtered', messageToSend);
         client.publish(this.topic + '/filtered', messageToSend);
     }
 }
